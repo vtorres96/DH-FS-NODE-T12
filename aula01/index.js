32a6dfe014f35e0c3864605dfdafcec49b8d9d23
muriloignaccio <murilohsignacio@gmail.com>
2020-10-19 21:07:37 -0300

    modificar texto dos consoles da aula01

--- aula01/index.js
+++ aula01/index.js
@@ -1,2 +1,2 @@
-console.log("Hello World! Mãe tô na DH lecionando para uma galera massa")
-console.log("Roi, programadores full stack, né? rs")
+console.log("Hello World! Mãe tô na DH lecionando para uma galera MASSA")
+console.log("Roi, programadores full stack, né? rsrsrsrsrsrsrsrsrs")
