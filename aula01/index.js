817f82a78f045fe91c44904dd9e619a2ea5f879c
vtorres96 <victorcdc96@gmail.com>
2020-10-19 21:32:09 -0300

    adicionando mais um console.log

--- aula01/index.js
+++ aula01/index.js
@@ -1,3 +1,4 @@
 console.log("Hello World! Mãe tô na DH lecionando para uma galera MASSA")
 console.log("Roi, programadores full stack, né? rsrsrsrsrsrsrsrsrs")
 console.log("sou a linha três deste arquivo")
+console.log("agora vai ai mais um conflito hehehe")
