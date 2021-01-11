aa7b5a0a57f4775223bdd7312a905375c298de70
vtorres96 <victorcdc96@gmail.com>
2021-01-11 20:06:28 -0300

    adicionando aula21 inicio

---
+++ aula21/data/card.js
@@ -0,0 +1,10 @@
+let cards = [
+  {
+    'caminho_de_imagem': '/images/card-1.jpg',
+    'titulo': 'Titulo Um', 
+    'texto': 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.', 
+    'botao': 'Ver mais'
+  }
+];
+
+module.exports = cards;
