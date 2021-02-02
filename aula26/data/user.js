eed527d4d380ceef10f9002d85cea6dc5398b464
vtorres96 <victorcdc96@gmail.com>
2021-02-01 21:50:41 -0300

    adicionada aula26: session

---
+++ aula26/data/user.js
@@ -0,0 +1,1 @@
+module.exports = [{"id":1,"name":"Novo","email":"novo@gmail.com"},{"id":2,"name":"Victor T Moreno","email":"victorcdc96@gmail.com","password":"$2b$10$YTM5D0VMIGkZ6x1lewufoOXqxcfWm47c0tb22GxVRsemKjqZxQLnW"}]
