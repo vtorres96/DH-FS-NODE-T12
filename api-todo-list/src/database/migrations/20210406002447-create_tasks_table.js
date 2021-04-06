bd0b3e2223eebe89120e6b1ba38cf6d1196b2cec
vtorres96 <victorcdc96@gmail.com>
2021-04-05 21:55:01 -0300

    aula 37: adicionando migration tasks

---
+++ api-todo-list/src/database/migrations/20210406002447-create_tasks_table.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('tasks', { 
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+		    allowNull: false,
+      },
+      title: Sequelize.STRING,
+      description: Sequelize.STRING,
+      done: Sequelize.BOOLEAN,
+	    deleted: Sequelize.BOOLEAN,
+      createdAt: Sequelize.DATE,
+      updatedAt: Sequelize.DATE,
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('tasks');
+  }
+};
