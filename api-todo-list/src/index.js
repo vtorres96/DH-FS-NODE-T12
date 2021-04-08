af8902fa50e8debb9c7ec03423dc1c102d1d850a
vtorres96 <victorcdc96@gmail.com>
2021-04-07 21:25:12 -0300

    aula 38: alterando a porta do servidor de 3000 para 3333

--- api-todo-list/src/index.js
+++ api-todo-list/src/index.js
@@ -9,6 +9,6 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(3000, () => {
-  console.log('Servidor rodando na porta http://localhost:3000')
+app.listen(3333, () => {
+  console.log('Servidor rodando na porta http://localhost:3333')
 })
