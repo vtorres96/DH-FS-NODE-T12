1ee738c38ee82007ed4cb53e60dad7c453314cb9
vtorres96 <victorcdc96@gmail.com>
2021-04-07 22:07:17 -0300

    aula 38: adicionando rota de listar tasks

--- api-todo-list/src/routes/index.js
+++ api-todo-list/src/routes/index.js
@@ -3,6 +3,7 @@ const router = Router()
 
 const TaskController = require('../controllers/TaskController')
 
+router.get('/api/task/list', TaskController.index)
 router.post('/api/task/create', TaskController.create)
 
 module.exports = router
