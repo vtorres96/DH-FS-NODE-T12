5f415f1ac516fafcb83978ed4b824a7fe270cb53
vtorres96 <victorcdc96@gmail.com>
2021-04-12 21:14:41 -0300

    aula 39: criando rotas e metodos getById, update e delete

--- api-todo-list/src/controllers/TaskController.js
+++ api-todo-list/src/controllers/TaskController.js
@@ -19,9 +19,57 @@ module.exports = {
   async create(req, res, next){
     try {
       let task = await Task.create({ ...req.body })
-      return res.status(201).json(task);
+      return res.status(201).json(task)
     } catch (error) {
       return res.status(400).json({ message: 'Error: ' + error.message })
     }
+  },
+
+  async getById (req, res, next) { 
+    try {
+      let id = req.params.id
+      let task = await Task.findByPk(id)
+      return res.status(200).json(task)
+    } catch (error) {
+      return res.status(400).json({ message: 'Error:' + error.message })
+    }
+  },
+
+  async update (req, res, next) {
+    try {
+      let id = req.params.id
+      let { title, description, done = false } = req.body
+      
+      let task = await Task.findByPk(id)
+
+      if(!task) {
+        return res.status(200).json({ message: 'Task not found' })
+      }
+
+      task.update({
+        title,
+        description,
+        done
+      })
+      
+      return res.status(200).json(task)
+    } catch (error) {
+      return res.status(400).json({ message: 'Error:' + error.message })
+    }
+  },
+
+  async delete (req, res, next) {
+    let id = req.params.id
+    let task = await Task.findByPk(id)
+
+    if(!task) {
+      return res.status(200).json({ message: 'Task not found' })
+    }
+
+    task.update({
+      deleted: true
+    })
+
+    return res.status(200).json({ message: 'Task deleted' })
   },
 }
--- api-todo-list/src/routes/index.js
+++ api-todo-list/src/routes/index.js
@@ -5,5 +5,8 @@ const TaskController = require('../controllers/TaskController')
 
 router.get('/api/task/list', TaskController.index)
 router.post('/api/task/create', TaskController.create)
+router.get('/api/task/get-by-id/:id', TaskController.getById)
+router.put('/api/task/update/:id', TaskController.update)
+router.delete('/api/task/delete/:id', TaskController.delete)
 
 module.exports = router
