88faab744a1a43d7a4f235b01ec1665a9b4ec3dc
vtorres96 <victorcdc96@gmail.com>
2021-04-14 20:40:00 -0300

    aula 40: criando rotas para efetuar requisicao na API do JsonPlaceHolder

--- api-consulta-backend/src/routes/index.js
+++ api-consulta-backend/src/routes/index.js
@@ -1,6 +1,9 @@
 const { Router } = require('express')
 const router = Router()
 
-router.get('/api/photo', )
+const PhotoController = require('../controllers/PhotoController')
+
+router.get('/api/photo/:limit?', PhotoController.index)
+router.get('/api/photo/:id', PhotoController.getById)
 
 module.exports = router
