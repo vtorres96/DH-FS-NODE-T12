e95815f96985fb282141bbdb11d47e94bedfe15a
vtorres96 <victorcdc96@gmail.com>
2021-04-19 21:56:38 -0300

    aula 41: adicionando componente App

--- web-todo-list/src/App.js
+++ web-todo-list/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import Header from './components/Header'
+import TaskForm from './components/TaskForm'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="wrapper">
+        <Header />
+        <div className="main">
+          <TaskForm />
+        </div>  
+      </div>
     </div>
   );
 }
