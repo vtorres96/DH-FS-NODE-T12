6f137744e10e4519054b41f14e0c4e560fabd188
vtorres96 <victorcdc96@gmail.com>
2021-04-28 20:40:28 -0300

    aula 43: refatorando componente TaskList para obter dados da API utilizando useState e useEffect e importando componente Task

--- web-todo-list/src/components/TaskList/index.js
+++ web-todo-list/src/components/TaskList/index.js
@@ -1,36 +1,29 @@
+import React, { useState, useEffect } from 'react'
+import Task from '../Task'
+import api from '../../services/api'
 import './styles.css'
 
-import Check from '@material-ui/icons/Check'
-import Delete from '@material-ui/icons/Delete'
-import Edit from '@material-ui/icons/Edit'
-
 function TaskList(){
+  const [tasks, setTasks] = useState([])
 
-  let tasks = [
-    {"id": 1, "description": "descricao 1"},
-    {"id": 2, "description": "descricao 2"},
-    {"id": 3, "description": "descricao 3"},
-    {"id": 4, "description": "descricao 4"},
-    {"id": 5, "description": "descricao 5"},
-  ]
-
-  const deleteTask = (id) => {console.log(id)}
-  const editTask = (id) => {console.log(id)}
-  const doneTask = (id) => {console.log(id)}
+  useEffect(() => {
+    async function getTasks(){
+      try {
+        const { data } = await api.get('/api/task/list')
+        setTasks(data)
+      } catch (error) {
+        console.log('Ocorreu um erro ao efetuar requisição' + error)
+      }
+    }
+    getTasks()
+  }, [])
 
   return (
     <div>
       {tasks.length ? (
         <ul className="list-tasks">
           {tasks.map(task => {        
-            return <li className="list-item" key={task.id}>
-              <span>{task.description}</span>
-              <div>
-                <Delete className="btn-icons" onClick={() => deleteTask(task.id)} />
-                <Edit className="btn-icons" onClick={() => editTask(task.id)} />
-                <Check className="btn-icons" onClick={() => doneTask(task.id)} />
-              </div>
-            </li>
+            return <Task key={task.id} task={task}/>
           })}
         </ul>
       ) : (
