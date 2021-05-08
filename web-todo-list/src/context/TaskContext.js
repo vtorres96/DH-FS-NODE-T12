4940ede2f8e915f3ec684533079f616558ac6d4d
vtorres96 <victorcdc96@gmail.com>
2021-05-08 16:24:33 -0300

    projeto react conclusao: adicionando metodos de concluir, remover, adicionar e alterar tarefas no TaskContext

--- web-todo-list/src/context/TaskContext.js
+++ web-todo-list/src/context/TaskContext.js
@@ -5,6 +5,7 @@ export const TaskContext = createContext()
 
 const TaskContextProvider = (props) => {
   const [tasks, setTasks] = useState([])
+  const [editItem, setEditItem] = useState(null)
 
   useEffect(() => {
     async function getTasks(){
@@ -18,18 +19,55 @@ const TaskContextProvider = (props) => {
     getTasks()
   }, [])
 
+  const addTask = async (title, description) => {
+    try {
+      const { data } = await api.post('/api/task/create', {
+        title,
+        description
+      })
+      setTasks([...tasks, data])
+    } catch (error) {
+      console.log('Ocorreu um erro ao adicionar a tarefa: ' + error)
+    }
+  }
+
+  const editTask = async (id, title, description) => {
+    await api.put('/api/task/update/' + id, {
+      title, description
+    })
+    setEditItem(null)
+  }
+
+  const deleteTask = async (id) => {
+    try {
+      await api.delete('/api/task/delete/' + id)
+      const newTasks = tasks.filter(task => task.id !== id)
+      setTasks(newTasks)
+    } catch (error) {
+      console.log('Ocorreu um erro ao excluir a tarefa: ' + error)
+    }
+  }
+
+  const doneTask = async (id) => {
+    try {
+      await api.put('/api/task/update/' + id, { 
+        done: true
+      })
+      const newTasks = tasks.filter(task => task.id !== id)
+      setTasks(newTasks)
+    } catch (error) {
+      console.log('Ocorreu um erro ao concluir a tarefa: ' + error)
+    }
+  }
+
   const findTaskById = async (id) => {
     try {
       const { data } = await api.get('/api/task/get-by-id/' + id)
-      console.log(data)
+      setEditItem(data)
     } catch (error) {
       console.log('Ocorreu um erro ao obter o registro ' + error)
     }
   }
-
-  const deleteTask = (id) => {console.log(id)}
-
-  const doneTask = (id) => {console.log(id)}
 
   return (
     <TaskContext.Provider 
@@ -38,6 +76,9 @@ const TaskContextProvider = (props) => {
         deleteTask,
         findTaskById,
         doneTask,
+        addTask,
+        editItem,
+        editTask
       }}
     >
       {props.children}
