946d21579b20fac3ebf5e4a7408041ca0ddf1feb
vtorres96 <victorcdc96@gmail.com>
2021-05-08 16:23:55 -0300

    projeto react conclusao: adicionando metodos para adicionar e editar tarefa

--- web-todo-list/src/components/TaskForm/index.js
+++ web-todo-list/src/components/TaskForm/index.js
@@ -1,13 +1,54 @@
+import { useContext, useEffect, useState } from 'react';
+import { TaskContext } from '../../context/TaskContext'
 import './styles.css';
 
 function TaskForm(){
+  const { addTask, editItem, editTask } = useContext(TaskContext)
+
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+
+  useEffect(() => {
+    if(editItem){
+      setTitle(editItem.title)
+      setDescription(editItem.description)
+    }
+  }, [editItem])
+
+  const handleChangeTitle = (event) => {
+    setTitle(event.target.value)
+  }
+
+  const handleChangeDescription = (event) => {
+    setDescription(event.target.value)
+  }
+
+  const handleSubmit = async (event) => {
+    if(editItem){
+      editTask(editItem.id, title, description)
+    } else {
+      addTask(title, description)
+    }
+
+    setTitle('')
+    setDescription('')
+  }
+
+  const handleClear = (event) => {
+    event.preventDefault()
+    setTitle('')
+    setDescription('')
+  }
+
   return (
-    <form action="" className="form">
-      <input type="text" className="task-input" placeholder="Title"/>
-      <input type="text" className="task-input" placeholder="Description"/>
+    <form onSubmit={handleSubmit} className="form">
+      <input onChange={handleChangeTitle} value={title} type="text" className="task-input" placeholder="Title"/>
+      <input onChange={handleChangeDescription} value={description} type="text" className="task-input" placeholder="Description"/>
       <div className="buttons">
-        <button className="btn add-task-btn">Adicionar</button>
-        <button className="btn clear-btn">Limpar</button>
+        <button className="btn add-task-btn">
+          {editItem ? 'Editar' : 'Adicionar'}
+        </button>
+        <button onClick={handleClear} className="btn clear-btn">Limpar</button>
       </div>
     </form>
   )
